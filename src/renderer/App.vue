21c2dd37fc006af82b3c820eb0b35cb33a2d3349
savayer <sava0094@gmail.com>
2019-02-05 17:56:05 +0200

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <transition name="slide">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'passing-tests'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="wrapper">
+        <div class="container">
+            <h1>test</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'main'
+  }
+</script>
+
+<style lang="scss" src="./../assets/scss/styles.scss"></style>
+
